978e4be4d2632be55c3601a73a01b5e5c3404eb3
Santiago Cotto <scotto@goshops.ai>
2023-08-24 12:33:33 -0300

    fix

--- gs-sdk/src/api/content.js
+++ gs-sdk/src/api/content.js
@@ -29,7 +29,7 @@ export const getContentByContext = async (context, options, ev) => {
   const contents = result.loadNowContent;
   await Promise.all(contents.map(content => addContentToWebsite(content, ev)));
   const lazyLoadContent = result.lazyLoadContent;
-  await Promise.all(lazyLoadContent.map(content => getContent(undefined, content.key, options)));
+  await Promise.all(lazyLoadContent.map(content => getContent(undefined, content.key, options, ev)));
 
 };
 
@@ -93,7 +93,7 @@ function buildContextPayload(options){
 }
 async function addContentToWebsite(content, ev){
   console.log('addContentToWebsite', ev);
-  
+
   if (content && content.contentValue){
     const css = content.contentValue.css;
     const html = content.contentValue.html;
