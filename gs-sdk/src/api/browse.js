e8cc81c2e4db8232e85fed1f133bec57ce6f2a30
Santiago Cotto <scotto@goshops.ai>
2023-09-10 20:25:53 -0300

    sd

--- gs-sdk/src/api/browse.js
+++ gs-sdk/src/api/browse.js
@@ -1,20 +1,40 @@
+import { getToken } from '../utils/storage';
+
 export const install = (options) => {
     // Create a new script element
     const script = document.createElement('script');
     script.src = 'https://cdn.jsdelivr.net/npm/rrweb@latest/dist/record/rrweb-record.min.js';
 
     // Add an event listener to execute your code once the script is loaded
-    script.onload = () => {
+    script.onload = async () => {
         // Your recording code
         let events = [];
         
-        rrwebRecord.record({
+        rrwebRecord({
             emit(event) {
                 // push event into the events array
                 events.push(event);
                 console.log(events);
             },
         });
+
+        const token = getToken();
+
+        // this function will send events to the backend and reset the events array
+        async function save() {
+            const body = JSON.stringify({ events });
+            const response = await fetch(`https://browse.go-shops.workers.dev`, {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${token}`
+                },
+                body: body,
+              });  
+        }
+        
+        // save events every 10 seconds
+        setInterval(save, 10 * 1000);
     };
 
     // Append the script to the document to start downloading and executing it
