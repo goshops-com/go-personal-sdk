b9f3b2733c4fe359795f76abb08d6c456119640c
Santiago Cotto <scotto@goshops.ai>
2025-01-07 12:29:21 -0300

    fixes

--- gs-sdk/src/api/live.js
+++ gs-sdk/src/api/live.js
@@ -2,19 +2,19 @@ import { httpGet, httpPatch } from '../utils/http';
 
 
 export const liveGetVideo = async (videoId) => {
-    const url = `/addons/live/video?id=${videoId}`;
+    const url = `/addon/live/video?id=${videoId}`;
     const result = await httpGet(url);
     return result;
 }
 
 export const liveLikeVideo = async (videoId) => {
-    const url = `/addons/live/video/${videoId}/like`;
+    const url = `/addon/live/video/${videoId}/like`;
     const result = await httpPatch(url, {});
     return result;
 }
 
 export const liveUnlikeVideo = async (videoId) => {
-    const url = `/addons/live/video/${videoId}/unlike`;
+    const url = `/addon/live/video/${videoId}/unlike`;
     const result = await httpPatch(url, {});
     return result;
 }
