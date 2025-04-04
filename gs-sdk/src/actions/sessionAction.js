2e5f30b972f99eb2545030111b0eafa0835cea99
Santiago Cotto <scotto@goshops.ai>
2025-04-04 19:59:47 -0300

    fixes

--- gs-sdk/src/actions/sessionAction.js
+++ gs-sdk/src/actions/sessionAction.js
@@ -2,6 +2,17 @@ import { getSession } from '../utils/storage';
 
 import { getUrlParameter } from '../utils/dom';
 
+function uuidv4() {
+    try {
+      return self.crypto.randomUUID();
+    } catch (e) {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
+}
+
 function storeValue(key, value) {
     return fetch('https://kv.gopersonal.ai/' + key, {
         method: 'PUT',
@@ -19,8 +30,18 @@ async function _setSharedSession(key) {
 
 export const executeActions = (provider) => {
     const key = getUrlParameter('_gsSessionSet');
-    if (!key) {
+    const callback = getUrlParameter('callback');
+    if (!key || !callback) {
         return;
     }
-    _setSharedSession(key);
+
+    const id = uuidv4();
+
+    _setSharedSession(id);
+
+    if (callback) {
+        const redirectUrl = new URL(callback);
+        redirectUrl.searchParams.append('gsSessionId', id);
+        window.location.href = redirectUrl.toString();
+    }
 }
