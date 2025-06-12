59782c892545dfbe57cdd49b5a6914285611c4ff
Santiago Cotto <scotto@goshops.ai>
2025-06-12 10:41:02 -0300

    fixes

--- gs-sdk/src/utils/custom.js
+++ gs-sdk/src/utils/custom.js
@@ -1,17 +1,5 @@
-export const sendEvent = (key, value, customer) => {
-    try {
-    fetch('https://custom-events.gopersonal.ai/event', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            key: key,
-            value: value,
-            customer: customer
-        })
-    });
-    } catch (error) {
-        return null;
-    }
+import { httpPost} from './http'
+
+export const sendEvent = async (key, value, customer) => {
+    await httpPost(`/personal/content-event`, { key, value });
 };
